ac649708a57cb6cdd7739626658a2ce0fda65843
miaodi <miaodi1987@gmail.com>
2015-10-15 07:54:08 +0800

    Revert "Create spline.h"
    This reverts commit c7dbe1d0b9e493253d70a5e287933dba6b50a92a.

--- spline.h
+++
@@ -1,9 +0,0 @@
-#ifndef _spline_h
-#define _spline_h
-#endif
-
-
-namespace spline {
-	void DersBasisFuns(int i, double u, int p, double* U, int n, double** & ders);
-	int Findspan(int m, int p, double* U, double u);
-}
